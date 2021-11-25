39319661ddfbe990aa8b40a01057add5629de129
Documenter.jl <documenter@juliadocs.github.io>
2021-11-25 23:03:21 +0000

    build based on cce94b7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#PlasmaProperties.jl","page":"Index","title":"PlasmaProperties.jl","text":"","category":"section"},{"location":"index.html","page":"Index","title":"Index","text":"Documentation for PlasmaProperties.jl","category":"page"}]
+}
