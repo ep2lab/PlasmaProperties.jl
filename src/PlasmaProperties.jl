cce94b7f24b19d90218884244a423e71302efe81
Mario Merino <mario.merino@uc3m.es>
2021-11-26 00:01:45 +0100

    fixes

--- src/PlasmaProperties.jl
+++ src/PlasmaProperties.jl
@@ -3,11 +3,22 @@ module PlasmaProperties
 import PhysicalConstants.CODATA2018: c_0, ε_0, μ_0, k_B, m_e, e, m_u
 using Unitful
 
-"""
-    ω_ce(B)
+export f_ce, f_pe
 
-    Compute the cyclotron frequency of electrons in units of Hz.
 """
-ω_ce(B) = (e*B/(2π*m_e) |> u"Hz")
+    f_ce(B)
+
+    Compute the cyclotron frequency of electrons in MHz.
+"""
+f_ce(B) = (e*B/m_e /(2π) |> u"MHz")
+
+
+"""
+    f_pe(B)
+
+    Compute the plasma frequency of electrons in MHz.
+""" 
+f_pe(ne) = (sqrt(ne*e^2/(m_e*ε_0)) /(2π) |> u"MHz")
+
 
 end # module
